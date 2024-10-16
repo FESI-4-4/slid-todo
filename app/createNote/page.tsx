057ce4f62c7b3fe565e50c402bf161bea199bcaa
hanbin <rj2303@naver.com>
2024-10-16 18:16:33 +0900

    🎨 #1 style: 임시저장된글 불러오기 토스트 퍼블리싱

--- app/createNote/page.tsx
+++ app/createNote/page.tsx
@@ -33,6 +33,13 @@ export default function Page() {
           <p className="rounded-md bg-slate-100 p-1 text-slate-700 text-xs">To do</p>
           <p className="text-sm font-normal text-slate-700">할일</p>
         </div>
+        <div className="w-full bg-blue-50 text-blue-500 rounded-full py-2.5 px-3 flex gap-4 items-center mb-6">
+          <IconClose circleFill="fill-blue-500" className="cursor-pointer" />
+          <p className="font-semibold text-sm grow">임시 저장된 노트가 있어요. 저장된 노트를 불러오시겠어요?</p>
+          <button className="rounded-full bg-white border border-blue-500 text-blue-500 text-sm font-semibold py-2 px-4">
+            불러오기
+          </button>
+        </div>
         <hr />
         <form className="grow w-full h-fit relative flex flex-col">
           <div className="w-full relative h-7 my-3">
--- public/icons/IconClose.tsx
+++ public/icons/IconClose.tsx
@@ -1,11 +1,11 @@
 import { SVGProps } from 'react';
 
-const IconClose = ({ ...props }: SVGProps<SVGSVGElement>) => {
+const IconClose = ({ circleFill, ...props }: SVGProps<SVGSVGElement> & { circleFill?: string }) => {
   return (
     <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
-      <circle cx="12" cy="12" r="9" fill="#64748B" />
-      <path d="M8 8L16 16" stroke="#F8FAFC" stroke-width="1.8" stroke-linecap="round" />
-      <path d="M16 8L8 16" stroke="#F8FAFC" stroke-width="1.8" stroke-linecap="round" />
+      <circle cx="12" cy="12" r="9" fill="#64748B" className={circleFill} />
+      <path d="M8 8L16 16" stroke="#F8FAFC" strokeWidth="1.8" strokeLinecap="round" />
+      <path d="M16 8L8 16" stroke="#F8FAFC" strokeWidth="1.8" strokeLinecap="round" />
     </svg>
   );
 };
