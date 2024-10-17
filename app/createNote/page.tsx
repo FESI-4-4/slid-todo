5aa8b16a2be31a072b7fe71d9a5f0ea47fba5f2d
hanbin <rj2303@naver.com>
2024-10-17 17:21:13 +0900

    🩹 #1 chore: single quote로 변경

--- app/createNote/page.tsx
+++ app/createNote/page.tsx
@@ -15,86 +15,86 @@ import IconTextUnderline from '@/public/icons/IconTextUnderline';
 
 export default function Page() {
   return (
-    <main className="flex w-full">
-      <section className="w-10 h-screen"></section> {/* 링크 임베드 */}
-      <section className="max-w-[800px] w-full py-6 px-10 flex flex-col">
-        <div className="flex w-full items-center mb-4">
-          <h1 className="grow text-slate-900 font-semibold text-lg">노트 작성</h1>
-          <button className="py-3 px-5 text-blue-500 font-semibold text-sm mr-2">임시저장</button>
-          <button className="py-3 px-5 bg-blue-500 rounded-xl text-white font-semibold">작성 완료</button>
+    <main className='flex w-full'>
+      <section className='w-10 h-screen'></section> {/* 링크 임베드 */}
+      <section className='max-w-[800px] w-full py-6 px-10 flex flex-col'>
+        <div className='flex w-full items-center mb-4'>
+          <h1 className='grow text-slate-900 font-semibold text-lg'>노트 작성</h1>
+          <button className='py-3 px-5 text-blue-500 font-semibold text-sm mr-2'>임시저장</button>
+          <button className='py-3 px-5 bg-blue-500 rounded-xl text-white font-semibold'>작성 완료</button>
         </div>
-        <div className="flex w-full gap-1.5 mb-3">
-          <div className="flex justify-center items-center rounded-md bg-slate-800 w-6 h-6">
+        <div className='flex w-full gap-1.5 mb-3'>
+          <div className='flex justify-center items-center rounded-md bg-slate-800 w-6 h-6'>
             <IconFlag />
           </div>
-          <p className="font-medium text-base text-slate-800">목표</p>
+          <p className='font-medium text-base text-slate-800'>목표</p>
         </div>
-        <div className="flex w-full gap-2 mb-6">
-          <p className="rounded-md bg-slate-100 p-1 text-slate-700 text-xs">To do</p>
-          <p className="text-sm font-normal text-slate-700">할일</p>
+        <div className='flex w-full gap-2 mb-6'>
+          <p className='rounded-md bg-slate-100 p-1 text-slate-700 text-xs'>To do</p>
+          <p className='text-sm font-normal text-slate-700'>할일</p>
         </div>
-        <div className="w-full bg-blue-50 text-blue-500 rounded-full py-2.5 px-3 flex gap-4 items-center mb-6">
-          <IconClose circleFill="fill-blue-500" className="cursor-pointer" />
-          <p className="font-semibold text-sm grow">임시 저장된 노트가 있어요. 저장된 노트를 불러오시겠어요?</p>
-          <button className="rounded-full bg-white border border-blue-500 text-blue-500 text-sm font-semibold py-2 px-4">
+        <div className='w-full bg-blue-50 text-blue-500 rounded-full py-2.5 px-3 flex gap-4 items-center mb-6'>
+          <IconClose circleFill='fill-blue-500' className='cursor-pointer' />
+          <p className='font-semibold text-sm grow'>임시 저장된 노트가 있어요. 저장된 노트를 불러오시겠어요?</p>
+          <button className='rounded-full bg-white border border-blue-500 text-blue-500 text-sm font-semibold py-2 px-4'>
             불러오기
           </button>
         </div>
         <hr />
-        <form className="grow w-full h-fit relative flex flex-col">
-          <div className="w-full relative h-7 my-3">
+        <form className='grow w-full h-fit relative flex flex-col'>
+          <div className='w-full relative h-7 my-3'>
             <input
-              className="w-full text-lg font-medium focus-visible:outline-none"
-              placeholder="노트의 제목을 입력해주세요"
+              className='w-full text-lg font-medium focus-visible:outline-none'
+              placeholder='노트의 제목을 입력해주세요'
             />
-            <p className="absolute right-0 top-0 text-slate-800 font-medium text-xs">
-              {22}/<span className="text-blue-500">30</span>
+            <p className='absolute right-0 top-0 text-slate-800 font-medium text-xs'>
+              {22}/<span className='text-blue-500'>30</span>
             </p>
           </div>
           <hr />
-          <div className="w-full my-3">
-            <p className="text-slate-800 text-xs font-medium">
+          <div className='w-full my-3'>
+            <p className='text-slate-800 text-xs font-medium'>
               공백포함 : 총 {0}자 | 공백제외 : 총 {0}자
             </p>
           </div>
-          <div className="w-full rounded-full bg-slate-200 p-1 flex items-center gap-2 mb-4">
-            <div className="w-6 h-6 rounded-full bg-blue-500 flex justify-center items-center">
+          <div className='w-full rounded-full bg-slate-200 p-1 flex items-center gap-2 mb-4'>
+            <div className='w-6 h-6 rounded-full bg-blue-500 flex justify-center items-center'>
               <IconEmbed />
             </div>
-            <p className="grow text-base font-normal text-slate-800">link</p>
-            <div className="w-6 h-6 rounded-full flex justify-center items-center">
+            <p className='grow text-base font-normal text-slate-800'>link</p>
+            <div className='w-6 h-6 rounded-full flex justify-center items-center'>
               <IconClose />
             </div>
           </div>
-          <div className="grow">
+          <div className='grow'>
             <textarea
-              placeholder="이 곳을 클릭해 노트 작성을 시작해주세요"
-              className="resize-none w-full h-full focus-visible:outline-none text-slate-700"
+              placeholder='이 곳을 클릭해 노트 작성을 시작해주세요'
+              className='resize-none w-full h-full focus-visible:outline-none text-slate-700'
             />
           </div>
-          <div className="w-full border border-slate-200 rounded-full py-2.5 px-4 absolute bottom-0 bg-white flex gap-4">
-            <div className="flex gap-1">
-              <IconTextBold className="cursor-pointer hover:bg-slate-100" />
-              <IconTextItalics className="cursor-pointer hover:bg-slate-100" />
-              <IconTextUnderline className="cursor-pointer hover:bg-slate-100" />
+          <div className='w-full border border-slate-200 rounded-full py-2.5 px-4 absolute bottom-0 bg-white flex gap-4'>
+            <div className='flex gap-1'>
+              <IconTextBold className='cursor-pointer hover:bg-slate-100' />
+              <IconTextItalics className='cursor-pointer hover:bg-slate-100' />
+              <IconTextUnderline className='cursor-pointer hover:bg-slate-100' />
             </div>
-            <div className="flex gap-1">
-              <IconTextAlignLeft className="cursor-pointer hover:bg-slate-100" />
-              <IconTextAlignMiddle className="cursor-pointer hover:bg-slate-100" />
-              <IconTextAlignRight className="cursor-pointer hover:bg-slate-100" />
+            <div className='flex gap-1'>
+              <IconTextAlignLeft className='cursor-pointer hover:bg-slate-100' />
+              <IconTextAlignMiddle className='cursor-pointer hover:bg-slate-100' />
+              <IconTextAlignRight className='cursor-pointer hover:bg-slate-100' />
             </div>
-            <div className="flex gap-1">
-              <IconTextBulletPoint className="cursor-pointer hover:bg-slate-100" />
-              <IconTextNumberPoint className="cursor-pointer hover:bg-slate-100" />
-              <IconTextColor className="cursor-pointer hover:bg-slate-100" />
+            <div className='flex gap-1'>
+              <IconTextBulletPoint className='cursor-pointer hover:bg-slate-100' />
+              <IconTextNumberPoint className='cursor-pointer hover:bg-slate-100' />
+              <IconTextColor className='cursor-pointer hover:bg-slate-100' />
             </div>
-            <div className="grow flex justify-end">
-              <IconAddLink className="cursor-pointer hover:bg-slate-100" />
+            <div className='grow flex justify-end'>
+              <IconAddLink className='cursor-pointer hover:bg-slate-100' />
             </div>
-            <div className="absolute top-0 -translate-y-full w-full bg-blue-50 text-blue-500 rounded-full py-2.5 px-6 -ml-4 -mt-4 flex gap-2 items-center">
+            <div className='absolute top-0 -translate-y-full w-full bg-blue-50 text-blue-500 rounded-full py-2.5 px-6 -ml-4 -mt-4 flex gap-2 items-center'>
               <IconCheck />
-              <p className="font-semibold text-sm">
-                임시 저장이 완료되었습니다 <span className="text-xs font-medium">ㆍ {}초전</span>
+              <p className='font-semibold text-sm'>
+                임시 저장이 완료되었습니다 <span className='text-xs font-medium'>ㆍ {}초전</span>
               </p>
             </div>
           </div>
