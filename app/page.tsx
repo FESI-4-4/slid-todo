1934a5744d7f3b05ae8a43f52c92f2ef4f4d7093
hanbin <rj2303@naver.com>
2024-10-17 11:55:16 +0900

    ✨ #12 feat: 모달 공통컴포넌트 제작
    - context api 사용
    - 각 모달별로 ModalProvider를 감싸고, 중첩해야 할 경우 ModalProvider 안에 다시 ModalProvider를 사용

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,19 @@
+import { ModalContent, ModalProvider, ModalTrigger } from '@/components/common/Modal';
+
 export default function Home() {
   return (
     <div>
       <main>slid todo</main>
+      <ModalProvider>
+        <ModalTrigger>open</ModalTrigger>
+        <ModalContent>
+          modal
+          <ModalProvider>
+            <ModalTrigger>second open</ModalTrigger>
+            <ModalContent>second</ModalContent>
+          </ModalProvider>
+        </ModalContent>
+      </ModalProvider>
     </div>
   );
 }
