759fdf7e3e1bd8e319f235b1ac84c2c64ab1e548
hanbin <rj2303@naver.com>
2024-10-18 13:46:41 +0900

    🩹 #6 chore: lang attribute ko로 수정

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='kr'>
+    <html lang='ko'>
       <body>
         <NavBar />
         {children}
