8a4f6f95db3095c96454b4d662a444c98cdc910d
hanbin <rj2303@naver.com>
2024-10-19 14:35:54 +0900

    🐛 #16 fix: 미들웨어 수정
    - 서버에 도달한 request의 헤더에 Authorization 추가 후 rewrite 옵션 객체의 request 프로퍼티에 할당

--- middleware.ts
+++ middleware.ts
@@ -3,9 +3,12 @@ import { NextRequest, NextResponse } from 'next/server';
 export function middleware(request: NextRequest) {
   const accessToken = request.cookies.get('accessToken')?.value;
   const pathname = request.nextUrl.pathname;
+  request.headers.set('Authorization', `Bearer ${accessToken}`);
 
   return NextResponse.rewrite(new URL(pathname, process.env.NEXT_PUBLIC_BASE_URL), {
-    headers: { Authorization: `Bearer ${accessToken}` },
+    request: {
+      headers: request.headers,
+    },
   });
 }
 
