cad8d1ffb7fe831f233666f5df83951f8c6fe82a
mokoko <guswlso2@naver.com>
2024-10-19 12:35:56 +0900

    ✨ #36 feat: todoitem 관련 아이콘 추가

---
+++ public/icons/IconInactive.tsx
@@ -0,0 +1,9 @@
+import { SVGProps } from 'react';
+
+const IconInactive = (props: SVGProps<SVGSVGElement>) => (
+  <svg width='24' height='24' viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg' {...props}>
+    <rect x='3.5' y='3.5' width='17' height='17' rx='5.5' fill='white' stroke='#E2E8F0' />
+  </svg>
+);
+
+export default IconInactive;
---
+++ public/icons/IconNoteView.tsx
@@ -0,0 +1,13 @@
+import { SVGProps } from 'react';
+
+const IconNoteView = (props: SVGProps<SVGSVGElement>) => (
+  <svg width='24' height='24' viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg' {...props}>
+    <circle cx='12' cy='12' r='12' fill='#F8FAFC' />
+    <rect x='7.5' y='6.90039' width='9' height='10.3846' rx='1.38462' fill='#60A5FA' />
+    <path d='M9.57715 10.0156H14.4233' stroke='white' strokeWidth='0.969231' strokeLinecap='round' />
+    <path d='M9.57715 12.0918H14.4233' stroke='white' strokeWidth='0.969231' strokeLinecap='round' />
+    <path d='M9.57715 14.1699H14.4233' stroke='white' strokeWidth='0.969231' strokeLinecap='round' />
+  </svg>
+);
+
+export default IconNoteView;
