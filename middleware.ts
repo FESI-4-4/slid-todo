ba801b2ad56df155022be15381031ee79b9f89ed
hanbin <rj2303@naver.com>
2024-10-21 15:44:53 +0900

    🐛 #16 fix: 미들웨어 rewrite URL에 쿼리스트링 포함하도록 수정

--- middleware.ts
+++ middleware.ts
@@ -3,9 +3,10 @@ import { NextRequest, NextResponse } from 'next/server';
 export function middleware(request: NextRequest) {
   const accessToken = request.cookies.get('accessToken')?.value;
   const pathname = request.nextUrl.pathname;
+  const search = request.nextUrl.search;
   request.headers.set('Authorization', `Bearer ${accessToken}`);
 
-  return NextResponse.rewrite(new URL(pathname, process.env.NEXT_PUBLIC_BASE_URL), {
+  return NextResponse.rewrite(new URL(`${pathname}${search}`, process.env.NEXT_PUBLIC_BASE_URL), {
     request: {
       headers: request.headers,
     },
