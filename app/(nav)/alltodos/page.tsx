7bffc155d5d38f0da8c4030f675da2357f03c1ab
mokoko <guswlso2@naver.com>
2024-10-22 13:30:47 +0900

    💄 #45 style: 모든 일페이지 스타일

--- app/(nav)/alltodos/page.tsx
+++ app/(nav)/alltodos/page.tsx
@@ -1,13 +1,27 @@
+'use client';
+
+import AllTodoList from '@/components/allTodos/AllTodoList';
 import Filters from '@/components/allTodos/Filters';
 import HeaderAllTodos from '@/components/allTodos/HeaderAllTodos';
+import { getTodos, TodoItemData } from '@/lib/api/getTodos';
+import { useEffect, useState } from 'react';
 
-export default function alltodos() {
+export default function Alltodos() {
+  const [todos, setTodos] = useState<TodoItemData[]>([]);
+  useEffect(() => {
+    const fetchTodos = async () => {
+      const { todos } = await getTodos({ done: false, size: 20 });
+      setTodos(todos);
+    };
+    fetchTodos();
+  }, []);
   return (
     <div className='bg-slate-100'>
       <main className='mx-20 my-6 '>
         <HeaderAllTodos length={3} />
-        <div className='sm:p-6 p-4 bg-white rounded-xl flex-col gap-4'>
+        <div className='sm:p-6 p-4 bg-white rounded-xl flex flex-col gap-4'>
           <Filters />
+          <AllTodoList todos={todos} />
         </div>
       </main>
     </div>
