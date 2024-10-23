b3012b227b0fc2203b84695f344de8c14444d784
mokoko <guswlso2@naver.com>
2024-10-23 16:04:35 +0900

    ✨ #63 feat: 목표타이틀 컴포넌트

--- app/(nav)/notes/[goalId]/page.tsx
+++ app/(nav)/notes/[goalId]/page.tsx
@@ -1,10 +1,32 @@
 import PageContainer from '@/components/common/PageLayout/PageContainer';
 import PageHeader from '@/components/common/PageLayout/PageHeader';
+import NoteGoalTitle from '@/components/notes/NoteGoalTitle';
+
+const notes = [
+  {
+    todo: {
+      done: true,
+      title: 'Complete project documentation',
+      id: 1,
+    },
+    updatedAt: '2024-10-23T05:06:11.986Z',
+    createdAt: '2024-10-21T12:45:30.123Z',
+    title: 'Finish the remaining tasks',
+    id: 101,
+    goal: {
+      title: 'Project Completion',
+      id: 10,
+    },
+    userId: 42,
+    teamId: 'team_abc123',
+  },
+];
 
 export default function Notes({ params }: { params: { goalId: string } }) {
   return (
     <PageContainer>
       <PageHeader title='노트 모아보기' />
+      <NoteGoalTitle goal={notes[0].goal} />
     </PageContainer>
   );
 }
---
+++ components/notes/NoteGoalTitle.tsx
@@ -0,0 +1,15 @@
+import IconFlag from '@/public/icons/IconFlag';
+import { Goal } from '../common/TodoItem';
+
+const NoteGoalTitle = ({ goal }: { goal: Goal }) => {
+  return (
+    <div className='flex items-center gap-x-2 rounded-xl bg-white border border-slate-100 py-3.5 px-6'>
+      <div className='flex justify-center items-center rounded-md bg-slate-800 w-6 h-6'>
+        <IconFlag />
+      </div>
+      <h2 className='text-sm font-semibold'>{goal.title}</h2>
+    </div>
+  );
+};
+
+export default NoteGoalTitle;
