34fbf2d307865a22d5176770fbf7c9a459a53282
MinaRoh <00minha@gmail.com>
2024-10-23 18:46:31 +0900

    💄 #57 목표 별 할 일 임시 스타일 추가

--- components/dashboard/GoalTodoCard.tsx
+++ components/dashboard/GoalTodoCard.tsx
@@ -8,27 +8,27 @@ const GoalTodoCard = ({ goal }: { goal: Goal }) => {
   const dones = useTodosQuery(`${goal.id}-dones`, { goalId: goal.id, done: true, size: 5 }).data;
 
   return (
-    <div className='w-full flex-col items-center'>
+    <div className='w-full flex-col justify-center items-center'>
       <div className='flex justify-between'>
-        <p>{goal.title || '목표이름'}</p>
+        <p className='text-lg font-bold'>{goal.title || '목표이름'}</p>
         <button>+할일추가</button>
       </div>
-      <div>progressbar</div>
-      <div className='flex'>
-        <div className='w-full flex-col bg-orange-200'>
-          <p className='bold'>To do</p>
+      <div className='my-2 w-full'>progress bar</div>
+      <div className='flex gap-3'>
+        <div className='w-full flex-col'>
+          <p className='bold text-sm font-semibold'>To do</p>
           {todos?.todos.map((todo) => (
             <TodoItem data={todo} key={todo.id} />
           ))}
         </div>
-        <div className='w-full flex-col bg-green-200'>
-          <p className='bold'>Done</p>
+        <div className='w-full flex-col'>
+          <p className='bold text-sm font-semibold'>Done</p>
           {dones?.todos.map((todo) => (
             <TodoItem data={todo} key={todo.id} />
           ))}
         </div>
       </div>
-      <button>더보기/펼치기</button>
+      <button className='w-full'>더보기/펼치기</button>
     </div>
   );
 };
