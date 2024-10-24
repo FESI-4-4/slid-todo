5a519949684f8abd16d9e7a0df7bf66e8f889fd4
mokoko <guswlso2@naver.com>
2024-10-24 16:48:54 +0900

    💄 #70 style: todos 없을 때 메시지 출력

--- components/AllTodos/AllTodoList.tsx
+++ components/AllTodos/AllTodoList.tsx
@@ -6,6 +6,10 @@ interface AllTodoListProps {
 }
 
 const AllTodoList: React.FC<AllTodoListProps> = ({ todos }) => {
+  if (!todos.length) {
+    return <div className='flex justify-center items-center text-sm text-slate-500'>등록한 일이 없어요</div>;
+  }
+
   return (
     <div>
       <ul className='flex flex-col gap-y-2'>
