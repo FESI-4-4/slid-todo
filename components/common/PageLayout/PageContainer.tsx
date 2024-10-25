fed23e9c2ca4ff94dfbf2a47fc53d98195d41d01
mokoko <guswlso2@naver.com>
2024-10-25 09:25:54 +0900

    💄 #73 style: 무한 스크롤에 따른 ui 변화 개선

--- components/common/PageLayout/PageContainer.tsx
+++ components/common/PageLayout/PageContainer.tsx
@@ -10,8 +10,8 @@ interface PageContainerProps {
 
 const PageContainer = ({ children, className = '', containerClassName = '' }: PageContainerProps) => {
   return (
-    <div className={twMerge(clsx('bg-slate-100 w-full h-full', containerClassName))}>
-      <div className={twMerge(clsx('lg:px-20 sm:px-6 px-4 py-6 max-w-[792px]', className))}>{children}</div>
+    <div className={twMerge(clsx('bg-slate-100 w-full h-full overflow-auto', containerClassName))}>
+      <div className={twMerge(clsx('lg:px-20 sm:px-6 px-4 py-6 max-w-[792px] h-full', className))}>{children}</div>
     </div>
   );
 };
